8895995a6b88fdebb4f37a11bb8caf8caf07a258
DESKTOP-GV16N45\Alumno <sergioperezalcal@gmail.com>
2025-01-08 18:24:51 +0100

    enemy

--- Assets/Scripts/SistemaPatrulla.cs
+++ Assets/Scripts/SistemaPatrulla.cs
@@ -1,24 +1,54 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class SistemaPatrulla : MonoBehaviour
 {
     [SerializeField] private Transform ruta;
+
+    [SerializeField] private NavMeshAgent agent;
     
-    private List <Transform> listadoPuntos = new List <Transform>();
-    // Start is called before the first frame update
-    void Start()
-    {
-        foreach (Transform puntoRuta in ruta.transform)
-        {
+    private List <Vector3> listadoPuntos = new List <Vector3>();
 
-        }
-    }
+    private int indiceDestinoActual =0;
 
-    // Update is called once per frame
-    void Update()
+    private Vector3 destinoActual;
+    private void Awake()
     {
         
+        foreach (Transform puntoRuta in ruta.transform)
+        {
+            listadoPuntos.Add(puntoRuta.position);
+        }
+    }
+    void Start()
+    {
+        StartCoroutine(PatrullarYEsperar());
+    }
+
+    private IEnumerator PatrullarYEsperar()
+    {
+        while (true)
+        {
+            ClacularDestino();
+            agent.SetDestination(destinoActual);
+            yield return new WaitUntil( () => agent.remainingDistance <=0);//expresión lambda.
+
+            yield return new WaitForSeconds(5);
+        }
+
+    }
+
+    private void ClacularDestino()
+    {
+        indiceDestinoActual++;
+        if (indiceDestinoActual >= listadoPuntos.Count)
+        {
+            indiceDestinoActual = 0;
+        }
+
+        destinoActual = listadoPuntos[indiceDestinoActual];
     }
 }
